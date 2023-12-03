7505d45b1398f51d1745e55bc0c558d618e6eb77
xsy-max <1565412554@qq.com>
2023-12-03 21:58:12 +0800

    feat: 12.3修改bug

--- developer/demo01.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-if __name__ == '__main__':
-    str ='zhangsannihaojibacai'
-    str2 = str.split('n')
-    print(str2)
-
-               
--- pom_demo/test_case/test_pytest_login.py
+++ pom_demo/test_case/test_pytest_login.py
@@ -11,23 +11,22 @@ filepath = pathlib.Path(__file__)
 Project_Root_Path = filepath.parents[2].resolve()
 
 
-
-@allure.epic('实现生成工单业务流程')
-@pytest.mark.login_setuser1
-@pytest.mark.parametrize('users', 'admin')
-@pytest.mark.parametrize('pwd', '123456')
-def test_workflow(self, users, pwd):
-    try:
-        # print(data['username'])
-        edge = webdriver.Edge()
-        lg = login_page.LoginPage(edge)
-        lg.login(self, user=users, pwd=pwd)
-        lg2 = Home_page(edge)
-    except:
-        with open('../temp/生成工单业务流程.png','wb') as file:
-            file.write(edge.get_screenshot_as_png())
-        raise
-
+class test_case:
+    @allure.epic('实现生成工单业务流程')
+    @pytest.mark.login_setuser1
+    @pytest.mark.parametrize('users', 'admin')
+    @pytest.mark.parametrize('pwd', '123456')
+    def test_workflow(self, users, pwd):
+        try:
+            # print(data['username'])
+            edge = webdriver.Edge()
+            lg = login_page.LoginPage(edge)
+            lg.login(self, user=users, pwd=pwd)
+            lg2 = Home_page(edge)
+        except:
+            with open('../temp/生成工单业务流程.png', 'wb') as file:
+                file.write(edge.get_screenshot_as_png())
+            raise
 
 
 if __name__ == "__mian__":
