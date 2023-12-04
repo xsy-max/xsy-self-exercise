d8c9b8efa74498682bf320b74b3718aa55fc13e4
xsy-max <1565412554@qq.com>
2023-12-04 18:37:17 +0800

    feat: 12.4修改bug

--- pom_demo/test_case/test_pytest_login.py
+++ pom_demo/test_case/test_pytest_login.py
@@ -14,7 +14,8 @@ Project_Root_Path = filepath.parents[2].resolve()
 class test_case:
     @allure.epic('实现生成工单业务流程等一系列操作')
     @allure.title('测试标题：工单业务创建')
-    @pytest.mark.login_setuser1
+    @allure.description('这是描述')
+    # @pytest.mark.login_setuser
     @pytest.mark.parametrize('users', 'admin')
     @pytest.mark.parametrize('pwd', '123456')
     def test_workflow(self, users, pwd):
